39e5ed253eeee80639b8628776e019c091004c54
Glyn Hudson <glyn.hudson@gmail.com>
2022-08-19 00:46:22 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # samsung-modbus-mim-b19n
-Example script to read data from a Samsung Heat Pump or HVAC unit using MIM-B19N Modbus module
+
+Example python script to read and write data to a Samsung Heat Pump or HVAC unit using a [MIM-B19N Modbus module](https://www.samsung.com/uk/support/model/MIM-B19N/)
+
+## Tested with:
+
+- AE050RXYDEG-EU Gen6 ASHP
+- Raspberry Pi with [USB Modbus reader](https://shop.openenergymonitor.com/modbus-rs485-to-usb-adaptor/)
+- Python 3
+
+
+## Install python module
+
+```
+$ pip3 install minimalmodbus
+```
+
+## Run with
+
+
+```
+$ python3 samsung-modbus.py
+```
+
+## Example output
+
+```
+Central heating status: 0
+Target indoor temp: 21.0
+Indoor temp: 22.7
+Target flow temp: 25.0
+Flow temp: 32.7
+Return temp: 33.6
+DHW status: 1
+DHW target temp: 55.0
+DHW temp: 49.8
+Away mode status: 0
+```
+
+## Control commands
+
+Write / controll comands can be activated by un-commenting them.
+
+## Next setps
+
+- Integrated this into a [EmonHub](https://github.com/openenergymonitor/emonhub) interfacer module to log the data to MQTT and [Emoncms](https://github.com/emoncms/emoncms)
+- Home Assistant integration? (can anyone help with this?)
+- NodeRED module? (can anyone help with this?)
+
+
+
+
+
---
+++ samsung-modbus.py
@@ -0,0 +1,106 @@
+# Example script to read data from a Samsung Heat Pump or HVAC unit using MIM-B19N Modbus module
+
+import minimalmodbus
+import serial
+import struct
+import time
+def millis():
+    return int(round(time.time() * 1000))
+
+def C(val):
+    return struct.pack('!H', val)
+
+instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+
+instrument.serial.port                     # this is the serial port name
+instrument.serial.baudrate = 9600          # Baud
+instrument.serial.bytesize = 8
+instrument.serial.parity   = serial.PARITY_EVEN
+instrument.serial.stopbits = 1
+instrument.serial.timeout  = 1             # seconds
+instrument.debug = False
+
+instrument.address = 1                     # this is the slave address number
+instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
+
+
+################################################################
+# READ COMMANDS
+################################################################
+
+dhw_temp = round(0.1*(instrument.read_register(75,functioncode=3)),2)
+time.sleep(0.5)
+
+return_temp = round(0.1*(instrument.read_register(65,functioncode=3)),2)
+time.sleep(0.5)
+
+flow_temp = round(0.1*(instrument.read_register(66,functioncode=3)),2)
+time.sleep(0.5)
+
+target_flow_temp = round(0.1*(instrument.read_register(68,functioncode=3)),2)
+time.sleep(0.5)
+
+dhw_status = instrument.read_register(72,functioncode=3)
+time.sleep(0.5)
+
+target_dhw_temp = round(0.1*(instrument.read_register(74,functioncode=3)),2)
+time.sleep(0.5)
+
+away_status = instrument.read_register(79,functioncode=3)
+time.sleep(0.5)
+
+ch_status = instrument.read_register(52,functioncode=3)
+time.sleep(0.5)
+
+indoor_temp = round(0.1*(instrument.read_register(59,functioncode=3)),2)
+time.sleep(0.5)
+
+target_indoor_temp  = round(0.1*(instrument.read_register(58,functioncode=3)),2)
+time.sleep(0.5)
+
+defrost_status = instrument.read_register(2,functioncode=3)
+
+
+print ("Central heating status: " + str(ch_status))
+print ("Target indoor temp: " + str(target_indoor_temp))
+print ("Indoor temp: " + str(indoor_temp))
+
+
+print ("Target flow temp: " + str(target_flow_temp))
+print ("Flow temp: " + str(return_temp))
+print ("Return temp: " + str(flow_temp))
+
+
+
+print ("DHW status: " + str(dhw_status))
+print ("DHW target temp: " + str(target_dhw_temp))
+print ("DHW temp: " + str(dhw_temp))
+
+
+print ("Away mode status: " + str(away_status))
+print ("Defrost operation status: " + str(defrost_status))
+
+
+
+################################################################
+# WrITE COMMANDS (CONTROL)
+################################################################
+
+# Switch on DHW
+# instrument.write_register(72,1)
+
+# Switch on CH
+# instrument.write_register(52,1)
+
+
+# Set DHW temp to 50 deg C
+# instrument.write_register(74,500)
+
+# Set flow temp to 40 deg C - doenst seem to work
+# instrument.write_register(68,400)
+
+# Set indoor target temp to 21 deg C
+# instrument.write_register(58,210)
+
+# Set away mode on
+# instrument.write_register(79,1)
